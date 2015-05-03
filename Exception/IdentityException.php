18d32a4352bdbda71252e56b733855d7b9b615c3
Baptiste Langlade <baptouuuu@gmail.com>
2015-05-03 12:20:01 +0200

    add identity map

---
+++ Exception/IdentityException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Innmind\Neo4j\ONM\Exception;
+
+class IdentityException extends \Exception
+{
+    const ALIAS_ALREADY_USED = 1;
+}
---
+++ Tests/IdentityMapTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Innmind\Neo4j\ONM\Tests;
+
+use Innmind\Neo4j\ONM\IdentityMap;
+
+class IdentityMapTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddClass()
+    {
+        $i = new IdentityMap;
+
+        $this->assertEquals($i, $i->addClass('stdClass'));
+        $this->assertTrue($i->has('stdClass'));
+    }
+
+    public function testAddAlias()
+    {
+        $i = new IdentityMap;
+
+        $i->addClass('stdClass');
+
+        $this->assertEquals($i, $i->addAlias('foo', 'stdClass'));
+        $this->assertTrue($i->has('stdClass'));
+        $this->assertTrue($i->has('foo'));
+    }
+
+    /**
+     * @expectedException Innmind\Neo4j\ONM\Exception\IdentityException
+     */
+    public function testThrowIfAliasAlreadyUsed()
+    {
+        $i = new IdentityMap;
+        $i->addAlias('foo', 'A');
+        $i->addAlias('foo', 'B');
+    }
+
+    public function testGetClass()
+    {
+        $i = new IdentityMap;
+        $i->addAlias('foo', 'stdClass');
+
+        $this->assertEquals('stdClass', $i->getClass('stdClass'));
+        $this->assertEquals('stdClass', $i->getClass('foo'));
+    }
+
+    public function testGetAlias()
+    {
+        $i = new IdentityMap;
+        $i->addAlias('foo', 'stdClass');
+
+        $this->assertEquals('foo', $i->getAlias('stdClass'));
+        $this->assertEquals('A', $i->getAlias('A'));
+    }
+}
