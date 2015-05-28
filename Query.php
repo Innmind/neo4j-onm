9435ea90a83101b3e81137d25ae3186a1e18fad5
Baptiste Langlade <baptouuuu@gmail.com>
2015-05-28 08:32:59 +0200

    improve query

--- Query.php
+++ Query.php
@@ -5,12 +5,27 @@ namespace Innmind\Neo4j\ONM;
 class Query
 {
     protected $cypher;
-    protected $params;
+    protected $params = [];
+    protected $types = [];
+    protected $variables = [];
 
-    public function __construct($cypher, array $params = null)
+    public function __construct($cypher = null)
     {
         $this->cypher = (string) $cypher;
-        $this->params = $params;
+    }
+
+    /**
+     * Set the cypher string
+     *
+     * @param string $cypher
+     *
+     * @return Query self
+     */
+    public function setCypher($cypher)
+    {
+        $this->cypher = (string) $cypher;
+
+        return $this;
     }
 
     /**
@@ -21,6 +36,26 @@ class Query
     public function getCypher()
     {
         return $this->cypher;
+    }
+
+    /**
+     * Add parameters to the query
+     *
+     * @param string $key
+     * @param mixed $params
+     * @param mixed $types Types associated to the parameters
+     *
+     * @return Query self
+     */
+    public function addParameters($key, $params, $types = null)
+    {
+        $this->params[(string) $key] = $params;
+
+        if ($types !== null) {
+            $this->types[(string) $key] = $types;
+        }
+
+        return $this;
     }
 
     /**
@@ -51,5 +86,60 @@ class Query
     public function __toString()
     {
         return $this->getCypher();
+    }
+
+    /**
+     * Register a variable with its entity alias used in the query
+     *
+     * @param string $variable
+     * @param string $alias
+     *
+     * @return Query self
+     */
+    public function addVariable($variable, $alias)
+    {
+        $this->variables[(string) $variable] = (string) $alias;
+
+        return $this;
+    }
+
+    /**
+     * Check if variables are defined
+     *
+     * @return bool
+     */
+    public function hasVariables()
+    {
+        return !empty($this->variables);
+    }
+
+    /**
+     * Return all the variables used in the query
+     *
+     * @return array
+     */
+    public function getVariables()
+    {
+        return $this->variables;
+    }
+
+    /**
+     * Check if the query has parameters types
+     *
+     * @return bool
+     */
+    public function hasTypes()
+    {
+        return !empty($this->types);
+    }
+
+    /**
+     * Return the parameters types
+     *
+     * @return array
+     */
+    public function getTypes()
+    {
+        return $this->types;
     }
 }
--- Tests/QueryTest.php
+++ Tests/QueryTest.php
@@ -6,7 +6,7 @@ use Innmind\Neo4j\ONM\Query;
 
 class QueryTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetQuery()
+    public function testSetQuery()
     {
         $q = new Query('MATCH (n)');
 
@@ -18,27 +18,61 @@ class QueryTest extends \PHPUnit_Framework_TestCase
             'MATCH (n)',
             (string) $q
         );
+
+        $q = new Query;
+
+        $this->assertSame(
+            $q,
+            $q->setCypher('MATCH (n2)')
+        );
+        $this->assertSame(
+            'MATCH (n2)',
+            (string) $q
+        );
     }
 
-    public function testHasParameters()
+    public function testSetParameters()
     {
-        $q = new Query('');
-        $this->assertFalse($q->hasParameters());
+        $q = new Query;
 
-        $q = new Query('', []);
         $this->assertFalse($q->hasParameters());
-
-        $q = new Query('', ['foo' => 'bar']);
+        $this->assertSame(
+            $q,
+            $q->addParameters('foo', 'bar')
+        );
         $this->assertTrue($q->hasParameters());
-    }
-
-    public function testGetParameters()
-    {
-        $q = new Query('', ['foo' => 'bar']);
-
         $this->assertSame(
             ['foo' => 'bar'],
             $q->getParameters()
+        );
+    }
+
+    public function testSetTypes()
+    {
+        $q = new Query;
+
+        $this->assertFalse($q->hasTypes());
+        $q->addParameters('foo', 'bar', 'string');
+        $this->assertTrue($q->hasTypes());
+        $this->assertSame(
+            ['foo' => 'string'],
+            $q->getTypes()
+        );
+    }
+
+    public function testSetVariable()
+    {
+        $q = new Query;
+
+        $this->assertFalse($q->hasVariables());
+        $this->assertSame(
+            $q,
+            $q->addVariable('n', 'Entity\Class')
+        );
+        $this->assertTrue($q->hasVariables());
+        $this->assertSame(
+            ['n' => 'Entity\Class'],
+            $q->getVariables()
         );
     }
 }
