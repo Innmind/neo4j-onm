f3a7f71fb1724524adac6f131d00226df1408fa6
Baptiste Langlade <baptouuuu@gmail.com>
2016-02-23 08:26:18 +0100

    add metadata property

---
+++ Tests/Metadata/PropertyTest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\ONM\Tests\Metadata;
+
+use Innmind\Neo4j\ONM\Metadata\Property;
+use Innmind\Neo4j\ONM\TypeInterface;
+
+class PropertyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $p = new Property(
+            'foo',
+            $t = $this->getMock(TypeInterface::class)
+        );
+
+        $this->assertSame('foo', $p->name());
+        $this->assertSame($t, $p->type());
+    }
+}
