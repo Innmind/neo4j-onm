c84a7154cfe65b0a1f26587323a2d17e3d69ed4e
Baptiste Langlade <baptouuuu@gmail.com>
2016-03-05 16:02:17 +0100

    add class to hold entities metadata

---
+++ Metadatas.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\ONM;
+
+use Innmind\Neo4j\ONM\Metadata\EntityInterface;
+use Innmind\Immutable\Map;
+
+class Metadatas
+{
+    private $aliases;
+    private $mapping;
+
+    public function __construct()
+    {
+        $this->aliases = new Map('string', 'string');
+        $this->mapping = new Map('string', EntityInterface::class);
+    }
+
+    /**
+     * Add a new entity metadata
+     *
+     * @param EntityInterface $meta
+     *
+     * @return self
+     */
+    public function add(EntityInterface $meta): self
+    {
+        $this->aliases = $this->aliases->put(
+            (string) $meta->alias(),
+            (string) $meta->class()
+        );
+        $this->mapping = $this->mapping->put((string) $meta->class(), $meta);
+
+        return $this;
+    }
+
+    /**
+     * Return the metadata for an entity
+     *
+     * @param string $name Class or alias
+     *
+     * @return EntityInterface
+     */
+    public function get(string $name): EntityInterface
+    {
+        if ($this->aliases->contains($name)) {
+            $name = $this->aliases->get($name);
+        }
+
+        return $this->mapping->get($name);
+    }
+}
---
+++ Tests/MetadatasTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\ONM\Tests;
+
+use Innmind\Neo4j\ONM\{
+    Metadatas,
+    Metadata\Alias,
+    Metadata\ClassName,
+    Metadata\EntityInterface
+};
+
+class MetadatasTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $m = new Metadatas;
+
+        $e = $this->getMock(EntityInterface::class);
+        $e
+            ->method('alias')
+            ->willReturn(new Alias('foo'));
+        $e
+            ->method('class')
+            ->willReturn(new ClassName('bar'));
+
+        $this->assertSame($m, $m->add($e));
+        $this->assertSame($e, $m->get('foo'));
+        $this->assertSame($e, $m->get('bar'));
+    }
+}
