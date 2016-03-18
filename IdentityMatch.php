1a315fe8163799fa0ad73662132678b036827e11
Baptiste Langlade <baptouuuu@gmail.com>
2016-03-18 08:12:58 +0100

    add identity match

---
+++ IdentityMatch.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\ONM;
+
+use Innmind\Neo4j\ONM\{
+    Metadata\EntityInterface,
+    Exception\InvalidArgumentException
+};
+use Innmind\Neo4j\DBAL\QueryInterface;
+use Innmind\Immutable\MapInterface;
+
+class IdentityMatch
+{
+    private $query;
+    private $variables;
+
+    public function __construct(
+        QueryInterface $query,
+        MapInterface $variables
+    ) {
+        if (
+            (string) $variables->keyType() !== 'string' ||
+            (string) $variables->valueType() !== EntityInterface::class
+        ) {
+            throw new InvalidArgumentException;
+        }
+
+        $this->query = $query;
+        $this->variables = $variables;
+    }
+
+    public function query(): QueryInterface
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return MapInterface<string, EntityInterface>
+     */
+    public function variables(): MapInterface
+    {
+        return $this->variables;
+    }
+}
