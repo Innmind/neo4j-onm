f2de7b3d5b0b043bcf0f2e20006f6c2bad0f458a
Baptiste Langlade <baptouuuu@gmail.com>
2016-03-20 14:34:57 +0100

    add visitor to build a property mapping out of the specification

---
+++ Translation/Specification/Visitor/PropertyMatchVisitorInterface.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\ONM\Translation\Specification\Visitor;
+
+use Innmind\Specification\SpecificationInterface;
+use Innmind\Immutable\MapInterface;
+
+interface PropertyMatchVisitorInterface
+{
+    /**
+     * Return a sequence composed of the property map and the associated parameters
+     *
+     * @param SpecificationInterface $specification
+     *
+     * @throws SpecificationNotApplicableAsPropertyMatchException
+     *
+     * @return MapInterface<string, SequenceInterface>
+     */
+    public function visit(SpecificationInterface $specification): MapInterface;
+}
